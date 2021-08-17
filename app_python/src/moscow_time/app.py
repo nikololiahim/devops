b0138a83074ab9a096bb7b603b68b853e2cf6986
nikololiahim <olomishcak@outlook.com>
2021-08-17 14:23:42 +0300

    app is now served through `waitress`

--- app_python/src/moscow_time/app.py
+++ app_python/src/moscow_time/app.py
@@ -1,4 +1,18 @@
+import logging
+
+import waitress
+from config import DEBUG, HOST, PORT
+
 from app_python.src.moscow_time import create_app
 
 if __name__ == "__main__":
-    create_app().run()
+    app = create_app()
+    if DEBUG:
+        app.logger.setLevel(logging.DEBUG)
+    app.logger.info(
+        f"""
+    Application running at {HOST}:{PORT}
+    Debug mode: {"on" if DEBUG else "off"}
+    """
+    )
+    waitress.serve(app, host=HOST, port=PORT)
