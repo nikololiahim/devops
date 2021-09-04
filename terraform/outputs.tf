68675f8993c4c8f977b67d6a57d529831e637123
nikololiahim <olomishcak@outlook.com>
2021-09-04 19:15:31 +0300

    terraform fmt

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 locals {
   ips = [
-  for k, v in var.language :
+    for k, v in var.language :
     {
-      language = k,
-      external_ip = google_compute_instance.instances[k].network_interface.0.access_config.0.nat_ip,
+      language     = k,
+      external_ip  = google_compute_instance.instances[k].network_interface.0.access_config.0.nat_ip,
       ssh_key_path = v
     }
   ]
@@ -12,15 +12,15 @@ locals {
 
 output "ips" {
   description = "A variable for extracting the external IP addresses of the instances"
-  value = local.ips
+  value       = local.ips
 }
 
 ### The Ansible inventory file
 resource "local_file" "AnsibleHosts" {
   content = templatefile("hosts.tmpl",
-  {
-    instance_data = local.ips
-  }
+    {
+      instance_data = local.ips
+    }
   )
   filename = "../ansible/inventory/hosts"
 }
