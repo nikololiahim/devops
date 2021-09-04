b460826ce7957ddc07ccb90689a168db8073619a
nikololiahim <olomishcak@outlook.com>
2021-09-04 18:55:32 +0300

    outputs.tf generates `hosts` file for ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,26 @@
+locals {
+  ips = [
+  for k, v in var.language :
+    {
+      language = k,
+      external_ip = google_compute_instance.instances[k].network_interface.0.access_config.0.nat_ip,
+      ssh_key_path = v
+    }
+  ]
+}
+
+
 output "ips" {
   description = "A variable for extracting the external IP addresses of the instances"
-  value = [
-    for k, _ in var.language :
-    [k, google_compute_instance.instances[k].network_interface.0.access_config.0.nat_ip]
-  ]
+  value = local.ips
+}
+
+### The Ansible inventory file
+resource "local_file" "AnsibleHosts" {
+  content = templatefile("hosts.tmpl",
+  {
+    instance_data = local.ips
+  }
+  )
+  filename = "../ansible/inventory/hosts"
 }
